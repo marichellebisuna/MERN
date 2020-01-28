4ec78c214c9efa88f40cf65f43cf66531d1b02f5
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-01-28 16:02:27 +1100

    user signup created with validator

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator =  require('express-validator');
+
 require("dotenv").config();
 
 //import routes
@@ -11,9 +16,16 @@ const app = express();
 //db
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useUnifiedTopology:true
 })
 .then(()=>console.log("Database connected."))
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
 
 //routes middleware
 app.use('/api',userRoutes);
