96ea0694e9bc0f29b23e9311927a33d88edee30a
Marichelle Bisuna <marichellebisuna@yahoo.com>
2020-01-28 17:26:02 +1100

    user signin jwt/authorization

--- models/user.js
+++ models/user.js
@@ -49,6 +49,9 @@ userSchema.virtual('password')
         .get(function(){ return this._password})
 
 userSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
     encryptPassword:function(password){
         if(!password) return '';
         try{
